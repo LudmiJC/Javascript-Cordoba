b9685f0b54c395ac4e590ea5d4e3f68fcf7dbcac
LudmiJC <ludmilajacquelinecordoba@gmail.com>
2023-12-29 15:50:31 -0300

    practicas de js

---
+++ js/clases.js
@@ -0,0 +1,24 @@
+//clase 1 Javascript//
+
+// let nombre = "ludmi"
+//let nombreEmpresa = "Fedmilnah solutions"
+//let edad = 21
+//const curso = "javascript"
+//let numeroDecimal
+
+/*nombreEmpresa = camel case
+/*nombre_empresa = snake case (underscore)
+
+//let nombre = promt ("ingresa tu nombre de pila:")
+
+//alert ("Bienvenid@: + nombre") 
+o
+//console.log ("bienvenid@:", nombre)
+/*ejemplo*/ 
+/*x = 15 + 3 
+/*x = 18 
+/*
+
+let respuesta = confirm ("¿desea formatear el disco rigido")
+console.log ("respuesta del usuario:", respuesta)
+
